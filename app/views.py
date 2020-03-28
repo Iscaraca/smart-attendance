56eeb3341041b8adbb79a03a951ce02e457e69a1
Isaac Chen <49803282+Iscaraca@users.noreply.github.com>
2020-03-29 01:04:40 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -21,7 +21,7 @@ def login():
             if account_form.validate_on_submit():
                 fullname = account_form.fullname.data
                 username = account_form.username.data
-                exists = Account.query.filter_by(username=username).first()
+                exists = Account.query.filter_by(username=username).first() # Checks database to see if username already exists
                 if not exists:
                     account = Account(fullname, username)
                     db.session.add(account)
@@ -33,11 +33,14 @@ def login():
 
 @app.route('/about/')
 def about():
-    """Render the website's about page."""
     return render_template('about.html')
 
 @app.route('/users')
 def show_users():
+    '''
+    SELECT * FROM users, gets each row as an object containing attributes for each column.
+    Stores objects into array users.
+    '''
     users = User.query.all()
 
     return render_template('show_users.html', users=users)
@@ -48,14 +51,19 @@ def add_user():
 
     if request.method == 'POST':
         if user_form.validate_on_submit():
-            # Get validated data from form
+            # Get current time
             now = datetime.now(timezone("Singapore"))
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            
+            # SELECT fullname FROM accounts WHERE username = user_form.name.data
             namefull = Account.query.filter_by(username=user_form.name.data).first()
+            
+            # Convert current day into integer, decode in base62 for validation
             dt_day = 10000*now.year + 100*now.month + now.day
             valid = base62.decode(str(dt_day))
+            
             if user_form.valid.data == str(valid):
-            # save user's attendance time to database
+            # Save user's name, class and attendance time to database
                 user = User(name = namefull.fullname, classno = user_form.classno.data , attendanceTime = dt_string)
                 db.session.add(user)
                 db.session.commit()
@@ -77,6 +85,7 @@ def flash_errors(form):
             
 @app.route('/password')
 def valid():
+    # Convert current day into integer, decode in base62 for validation
     now = datetime.now(timezone("Singapore"))
     dt_day = 10000*now.year + 100*now.month + now.day
     valid = base62.decode(str(dt_day))
@@ -85,17 +94,17 @@ def valid():
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
-    """Send your static text file."""
+    # Send static text file for future expansion
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
 
 @app.after_request
 def add_header(response):
-    """
+    '''
     Add headers to both force latest IE rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
-    """
+    '''
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
@@ -103,7 +112,7 @@ def add_header(response):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
+    # Custom 404 page handler
     return render_template('404.html'), 404
 
 
